a0c72583ad6dc72540ee23a1a71928649ab37e41
john-ngarambe <51552292+john-ngarambe@users.noreply.github.com>
2020-01-11 22:18:37 -0500

    Changes have been done on project

--- src/com/ngarambe/java/BankProject.java
+++ src/com/ngarambe/java/BankProject.java
@@ -17,35 +17,98 @@ public class BankProject {
         System.out.println("Names : " + ct.customerFirstName + "  " + ct.customerLastName);
         System.out.println( "Balance on current account = "+ ct.getBalance());*/
 
-       CreateAccount account = new CreateAccount();
-       Mortgage mortgage = new Mortgage();
+       //CreateAccount account = new CreateAccount();
+       //Mortgage mortgage = new Mortgage();
 
-       account.createAccount();
-       account.deposit();
-       account.withdrawal();
-       account.displayDetails();
+       //account.createAccount();
+       //account.deposit();
+       //account.withdrawal();
+       //account.displayDetails();
 
+       // Scanner scanner = new Scanner(System.in);
+       // System.out.println("Enter loan amount :");
+        //int loanAmount = scanner.nextInt();
+       // System.out.println("Enter loan term(in years): ");
+       // int termInYears = scanner.nextInt();
+        //System.out.println("Enter interest rate : ");
+       // double interestRate = scanner.nextDouble();
+        //double monthlyPayment = mortgage.calculateMonthlyPayment(loanAmount,termInYears,interestRate);
+
+        //NumberFormat currencyFormat = NumberFormat.getCurrencyInstance();
+       // NumberFormat interestFormat = NumberFormat.getPercentInstance();
+
+        //System.out.println("Loan Amount : " + currencyFormat.format(loanAmount));
+       // System.out.println("Loan Term : " + termInYears+ " Years");
+       // System.out.println("Interest Rate= " +interestFormat.format(interestRate));
+      //  System.out.println("Monthly Payment = " + currencyFormat.format(monthlyPayment));
+       // System.out.println("Loan Balance = " + currencyFormat.format(mortgage.loanBalance));
+       // System.out.println("Principal of Loan payed = " + currencyFormat.format(mortgage.principal));
+       // System.out.println("Interest on loan payed per month = " + currencyFormat.format(mortgage.interestPerMonth));
+        CreateAccount account = new CreateAccount();
+        Mortgage mortgage = new Mortgage();
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter loan amount :");
-        int loanAmount = scanner.nextInt();
-        System.out.println("Enter loan term(in years): ");
-        int termInYears = scanner.nextInt();
-        System.out.println("Enter interest rate : ");
-        double interestRate = scanner.nextDouble();
-        double monthlyPayment = mortgage.calculateMonthlyPayment(loanAmount,termInYears,interestRate);
-
-        NumberFormat currencyFormat = NumberFormat.getCurrencyInstance();
-        NumberFormat interestFormat = NumberFormat.getPercentInstance();
-
-        System.out.println("Loan Amount : " + currencyFormat.format(loanAmount));
-        System.out.println("Loan Term : " + termInYears+ " Years");
-        System.out.println("Interest Rate= " +interestFormat.format(interestRate));
-        System.out.println("Monthly Payment = " + currencyFormat.format(monthlyPayment));
-        System.out.println("Loan Balance = " + currencyFormat.format(mortgage.loanBalance));
-        System.out.println("Principal of Loan payed = " + currencyFormat.format(mortgage.principal));
-        System.out.println("Interest on loan payed per month = " + currencyFormat.format(mortgage.interestPerMonth));
 
 
+       // System.out.println("Enter your choice :");
+        int choice ;
+        // System.out.println("Enter loan amount :");
+       // int loanAmount = scanner.nextInt();
+        // System.out.println("Enter loan term(in years): ");
+        //int termInYears = scanner.nextInt();
+        //System.out.println("Enter interest rate : ");
+        //double interestRate = scanner.nextDouble();
+        //double monthlyPayment = mortgage.calculateMonthlyPayment(loanAmount,termInYears,interestRate);
 
+        //NumberFormat currencyFormat = NumberFormat.getCurrencyInstance();
+        //NumberFormat interestFormat = NumberFormat.getPercentInstance();
+
+        do {
+            System.out.println("Enter your choice :");
+            System.out.println("1: Creating Account");
+            System.out.println("2: Making Deposit");
+            System.out.println("3: Withdraw on your Account");
+            System.out.println("4: Mortgage:");
+            System.out.println("5: Quit the program:\n");
+            choice = scanner.nextInt();
+            switch (choice) {
+                case 1:
+                    account.createAccount();
+                    scanner.nextInt();
+                    break;
+                case 2:
+                    account.deposit();
+                    account.displayDetails();
+                    scanner.nextInt();
+                    break;
+                case 3:
+                    account.withdrawal();
+                    account.displayDetails();
+                    scanner.nextInt();
+                    break;
+                case 4:
+                    System.out.println("Enter loan amount :");
+                   int loanAmount = scanner.nextInt();
+                    System.out.println("Enter loan term(in years): ");
+                   int termInYears = scanner.nextInt();
+                    System.out.println("Enter interest rate : ");
+                   double interestRate = scanner.nextDouble();
+                    double monthlyPayment = mortgage.calculateMonthlyPayment(loanAmount, termInYears, interestRate);
+                    NumberFormat currencyFormat = NumberFormat.getCurrencyInstance();
+                    NumberFormat interestFormat = NumberFormat.getPercentInstance();
+                    System.out.println("Loan Amount : " + currencyFormat.format(loanAmount));
+                    System.out.println("Loan Term : " + termInYears + " Years");
+                    System.out.println("Interest Rate= " + interestFormat.format(interestRate));
+                    System.out.println("Monthly Payment = " + currencyFormat.format(monthlyPayment));
+                    System.out.println("Loan Balance = " + currencyFormat.format(mortgage.loanBalance));
+                    System.out.println("Principal of Loan payed = " + currencyFormat.format(mortgage.principal));
+                    System.out.println("Interest on loan payed per month = " + currencyFormat.format(mortgage.interestPerMonth));
+                    scanner.nextInt();
+                    break;
+                case 5:
+                    break;
+                default: System.out.println("You Entered Wrong choice");
+
+            }
+        }while(choice != 5);
     }
 }
--- src/com/ngarambe/java/CreateAccount.java
+++ src/com/ngarambe/java/CreateAccount.java
@@ -9,7 +9,20 @@ public class CreateAccount extends Account {
         balance = initial;
     }
 
+    @Override
+    public void createAccount() {
+        super.createAccount();
+        System.out.println("Account is created successfully");
+        System.out.println("Account Owner :" + accountName);
+        System.out.println("Account Type : " + accountType);
+        System.out.println("Account number is :" + accountNumber);
+
+
+
+    }
+
     public CreateAccount() {
         super();
+
     }
 }
